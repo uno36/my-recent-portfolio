2cd1c875806b8bd59e6d8d0a82c017643536f24d
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-18 14:55:35 +0200

    Make Projects card component

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const ProjectCard = ({
+  title, description, imgUrl, projectLink,
+}) => (
+  <Col size={12} sm={6} md={4}>
+    <div className="proj-imgbx">
+      <img src={imgUrl} alt="projectImg" />
+      <div className="proj-txtx">
+        <h4>{title}</h4>
+        <span>{description}</span>
+      </div>
+    </div>
+    <a href={projectLink} target="_blank" rel="noopener noreferrer">
+      <button
+        type="button"
+        className="btn btn-primary mb-4"
+      >
+        See Project
+      </button>
+    </a>
+  </Col>
+);
+
+ProjectCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  projectLink: PropTypes.string.isRequired, // Add projectLink prop
+};
+
+export default ProjectCard;
